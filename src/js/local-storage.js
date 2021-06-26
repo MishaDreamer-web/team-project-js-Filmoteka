36b9a592f71b3073c2f2c40dcd0f068bd013f020
MishaDreamer-web <shondayn@gmail.com>
2021-06-26 19:15:29 +0300

    Update local-storage.js

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -20,17 +20,17 @@ if (localStorage.getItem('arrayOfQueue') !== null) {
 
 modal.addEventListener('click', function (event) {
   const el = document.querySelector('.film-img');
-  const targetId = el.getAttribute("id");
+  const targetId = el.getAttribute('id');
   localStorage.setItem('targetId', JSON.stringify(targetId));
-  
+
   if (event.target.nodeName === 'BUTTON') {
     if (event.target.className === 'add-to-watched') {
       arrayOfWatched.push(localStorage.getItem('targetId'));
       localStorage.setItem('arrayOfWatched', JSON.stringify(arrayOfWatched));
-    } if (event.target.className === 'add-to-queue') {
+    }
+    if (event.target.className === 'add-to-queue') {
       arrayOfQueue.push(localStorage.getItem('targetId'));
       localStorage.setItem('arrayOfQueue', JSON.stringify(arrayOfQueue));
-
     }
   } else {
     return console.log('мимо кассы');
@@ -41,23 +41,23 @@ watchedBtn.addEventListener('click', onWatchedBtn);
 
 function onWatchedBtn() {
   const allList = document.querySelectorAll('.gallery');
-    for (const list of allList) {
-      list.remove();
-    }
-    const watchedList = document.createElement('li');
-    watchedList.setAttribute('class', 'item');
-    main.appendChild(watchedList);
+  for (const list of allList) {
+    list.remove();
+  }
+  const watchedList = document.createElement('li');
+  watchedList.setAttribute('class', 'item');
+  main.appendChild(watchedList);
 
-    let watchedArray = localStorage.getItem('arrayOfWatched');
-    watchedArray = JSON.parse(watchedArray);
+  let watchedArray = localStorage.getItem('arrayOfWatched');
+  watchedArray = JSON.parse(watchedArray);
 
-    for (const object of watchedArray) {
-      const innerHTML = JSON.parse(object);
-      const li = document.createElement('li');
-      li.setAttribute('class', 'list__element');
-      li.insertAdjacentHTML('beforeend', innerHTML);
-      watchedList.appendChild(li);
-    }
+  for (const object of watchedArray) {
+    const innerHTML = JSON.parse(object);
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list__element');
+    li.insertAdjacentHTML('beforeend', innerHTML);
+    watchedList.appendChild(li);
+  }
   watchedBtn.removeEventListener('click', onWatchedBtn);
 }
 
@@ -65,24 +65,22 @@ queueBtn.addEventListener('click', onQueueBtn);
 
 function onQueueBtn() {
   const allList = document.querySelectorAll('.gallery');
-    for (const list of allList) {
-      list.remove();
-    }
-    const watchedList = document.createElement('li');
-    watchedList.setAttribute('class', 'item');
-    main.appendChild(watchedList);
+  for (const list of allList) {
+    list.remove();
+  }
+  const watchedList = document.createElement('li');
+  watchedList.setAttribute('class', 'item');
+  main.appendChild(watchedList);
 
-    let queueArray = localStorage.getItem('arrayOfQueue');
-    queueArray = JSON.parse(queueArray);
+  let queueArray = localStorage.getItem('arrayOfQueue');
+  queueArray = JSON.parse(queueArray);
 
-    for (const object of queueArray) {
-      const innerHTML = JSON.parse(object);
-      const li = document.createElement('li');
-      li.setAttribute('class', 'list__element');
-      li.insertAdjacentHTML('beforeend', innerHTML);
-      watchedList.appendChild(li);
-    }
+  for (const object of queueArray) {
+    const innerHTML = JSON.parse(object);
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list__element');
+    li.insertAdjacentHTML('beforeend', innerHTML);
+    watchedList.appendChild(li);
+  }
   queueBtn.removeEventListener('click', onQueueBtn);
 }
-});
-
